dba6cc84005b3ef3d613723aa8031ec9fcacafa0
Ziabeher Phillips <zabq02@gmail.com>
2022-06-28 12:27:42 -0400

    a lot of redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
